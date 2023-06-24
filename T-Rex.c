96e7f0562d98a913d510d494d923aff4f633bee2
name121125 <bojun423@gmail.com>
2023-06-24 12:01:07 +0800

    let dino run(non-finished)

--- T-Rex.c
+++ T-Rex.c
@@ -8,16 +8,17 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-#define WINDOW_WIDTH (395)
-#define WINDOW_HEIGHT (128)
+#define WINDOW_WIDTH (772)
+#define WINDOW_HEIGHT (250)
 
 int main() {
     int exit = 1;
+    Uint32 current_fps;
     SDL_Rect rect = {(WINDOW_WIDTH - 48)/2, (WINDOW_HEIGHT - 48)/2, 48, 48};
    
     SDL_Rect back_rect = {0, 12, WINDOW_WIDTH, WINDOW_HEIGHT};
-    SDL_Rect back_rect2 = {395, 12, WINDOW_WIDTH, WINDOW_HEIGHT};
-    SDL_Rect back_rect3 = {790, 12, WINDOW_WIDTH, WINDOW_HEIGHT};
+    SDL_Rect back_rect2 = {772, 12, WINDOW_WIDTH, WINDOW_HEIGHT};
+    SDL_Rect back_rect3 = {1544, 12, WINDOW_WIDTH, WINDOW_HEIGHT};
 
     SDL_Event event;
 
@@ -26,34 +27,39 @@ int main() {
     SDL_Renderer* render = NULL;
     SDL_Texture* background_texture = NULL;
     SDL_Surface* trex_surface = NULL;
+    SDL_Texture* trex_texture1 = NULL;
+    SDL_Texture* trex_texture2 = NULL;
     SDL_Texture* trex_texture = NULL;
 
     window = SDL_CreateWindow("T_Rex", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     	
     background_texture = IMG_LoadTexture(render, "res/background.png"); 
-    trex_texture = IMG_LoadTexture(render, "res/dino.png");
+    trex_texture = IMG_LoadTexture(render, "res/dino1.png");
+    trex_texture2 = IMG_LoadTexture(render,"res/dino.png");
 	
     while (exit) 
     {
 
 	SDL_PollEvent(&event);
 	if (event.type == SDL_QUIT) exit = 0;
+	current_fps = SDL_GetTicks();
+
+	back_rect.x -= 2;
+	back_rect2.x -= 2;
+	back_rect3.x -= 2;
 	
-	back_rect.x -= 3;
-	back_rect2.x -= 3;
-	back_rect3.x -= 3;
+	if (back_rect.x < -772) { back_rect.x = 1544; }
+	if (back_rect2.x < -772) { back_rect2.x = 1544; }
+	if (back_rect3.x < -772) { back_rect3.x = 1544; }
 	
-	if (back_rect.x < -395) { back_rect.x = 790; }
-	if (back_rect2.x < -395) { back_rect2.x = 790; }
-	if (back_rect3.x < -395) { back_rect3.x = 790; }
-	 
 	SDL_RenderClear(render);
 	SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
+	if (current_fps%2 == 0) {SDL_RenderCopy(render, trex_texture1, NULL, &rect);}
+	    else {SDL_RenderCopy(render, trex_texture2, NULL, &rect);}
 	SDL_RenderCopy(render, background_texture, NULL, &back_rect);
 	SDL_RenderCopy(render, background_texture, NULL, &back_rect2);
 	SDL_RenderCopy(render, background_texture, NULL, &back_rect3);
-        SDL_RenderCopy(render, trex_texture, NULL,&rect);
 	SDL_RenderPresent(render);
 	SDL_Delay(20);
     }
